719caf08453bb99247c4ca80ad58140756f9d449
leek <13624602024@163.com>
2018-08-23 12:04:01 +0800

    2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -45,6 +45,7 @@ void MainWindow::setUi()
 
     m_cb_vfactor = new QCheckBox("电压系数");
 
+    m_cb_jiemi = new QCheckBox("解密");
     m_name = new QLineEdit();
     QString currentDate = QDate::currentDate().toString("yyyyMMdd");
     m_data = new QLineEdit(currentDate.mid(2));
@@ -60,37 +61,51 @@ void MainWindow::setUi()
     gbox->addWidget(m_cb_vsd,4,0);
 
 
-    gbox->addWidget(m_name,0,1);
-    gbox->addWidget(m_data,1,1);
-    gbox->addWidget(m_ckCode,2,1);
-    gbox->addWidget(m_password,3,1);
+    gbox->addWidget(m_name,0,1,1,2);
+    gbox->addWidget(m_data,1,1,1,2);
+    gbox->addWidget(m_ckCode,2,1,1,2);
+    gbox->addWidget(m_password,3,1,1,2);
     gbox->addWidget(m_cb_vfactor,4,1);
+    gbox->addWidget(m_cb_jiemi,4,2);
 
 
-    QHBoxLayout *hbox_btn = new QHBoxLayout();
+    QVBoxLayout *vbox_btn = new QVBoxLayout();
     m_start = new QPushButton("1执行");
-    m_copy = new QPushButton("2拷贝hex");
-    m_check = new QPushButton("3检查");
-    m_compress = new QPushButton("4打包");
-    m_clear = new QPushButton("5清理");
+    m_build = new QPushButton("2编译");
+    m_download = new QPushButton("3下载");
+    m_copy = new QPushButton("4拷贝hex");
+    m_check = new QPushButton("5检查");
+    m_compress = new QPushButton("6打包");
+    m_clear = new QPushButton("7清理");
 
-    hbox_btn->addWidget(m_start);
-    hbox_btn->addWidget(m_copy);
-    hbox_btn->addWidget(m_check);
-    hbox_btn->addWidget(m_compress);
-    hbox_btn->addWidget(m_clear);
+    vbox_btn->addWidget(m_start);
+    vbox_btn->addWidget(m_build);
+    vbox_btn->addWidget(m_download);
+    vbox_btn->addWidget(m_copy);
+    vbox_btn->addWidget(m_check);
+    vbox_btn->addWidget(m_compress);
+    vbox_btn->addWidget(m_clear);
 
+    QHBoxLayout *hbox_btn1 = new QHBoxLayout();
+    m_modfiy_build = new QPushButton("修改程序-编译工程");
+    m_modfiy_build->setFixedHeight(50);
+    m_compress_clear = new QPushButton("打包程序-清理目录");
+    m_compress_clear->setFixedHeight(50);
+    hbox_btn1->addWidget(m_modfiy_build);
+    hbox_btn1->addWidget(m_compress_clear);
 
     m_loglist = new QListWidget;
     QHBoxLayout *hbox_set = new QHBoxLayout();
     hbox_set->addWidget(groupbox);
     hbox_set->addWidget(m_loglist);
-    hbox_set->setStretch(0,3);
-    hbox_set->setStretch(1,2);
+    hbox_set->addLayout(vbox_btn);
+    hbox_set->setStretch(0,5);
+    hbox_set->setStretch(1,3);
+    hbox_set->setStretch(2,1);
 
     vbox->addLayout(hbox);
     vbox->addLayout(hbox_set);
-    vbox->addLayout(hbox_btn);
+    vbox->addLayout(hbox_btn1);
 
 
     m_cb_data->setChecked(true);
@@ -98,11 +113,16 @@ void MainWindow::setUi()
     m_cb_password->setChecked(true);
 
     connect(m_start, SIGNAL(clicked(bool)), this, SLOT(on_m_start_clicked()));
+    connect(m_build, SIGNAL(clicked(bool)), this, SLOT(on_m_build_clicked()));
+    connect(m_download, SIGNAL(clicked(bool)), this, SLOT(on_m_download_clicked()));
     connect(m_check, SIGNAL(clicked(bool)), this, SLOT(on_m_check_clicked()));
     connect(m_copy, SIGNAL(clicked(bool)), this, SLOT(on_m_copy_clicked()));
     connect(m_compress, SIGNAL(clicked(bool)), this, SLOT(on_m_compress_clicked()));
     connect(m_clear, SIGNAL(clicked(bool)), this, SLOT(on_m_clear_clicked()));
     connect(btn, SIGNAL(clicked(bool)), this, SLOT(on_btn_clicked()));
+
+    connect(m_modfiy_build, SIGNAL(clicked(bool)), this, SLOT(on_build2_clicked()));
+    connect(m_compress_clear, SIGNAL(clicked(bool)), this, SLOT(on_clear2_clicked()));
 }
 
 void MainWindow::on_m_start_clicked()
@@ -132,7 +152,7 @@ void MainWindow::on_m_start_clicked()
         dir.setPath(new_path);
     }
 
-    m_loglist->addItem("1.删除原hex.");
+    m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("删除原hex."));
     foreach(QString filename ,dir.entryList())
     {
         qDebug() << filename;
@@ -144,17 +164,20 @@ void MainWindow::on_m_start_clicked()
         //3.解压软件包，并删除压缩包
         if (filename.endsWith("rar") || filename.endsWith("zip"))
         {
-            m_loglist->addItem("2.开始解压.");
+            qApp->processEvents();
+            m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("开始解压."));
             QString code_packet = dir.absolutePath()+"\/"+filename;
-            QString extract_cmd = QString("WinRaR.exe x -ibck -y -o+ %1 %2")
+            QString extract_cmd = QString("\"%1\" x -ibck -y -o+ \"%1\" \"%2\"")
+                            .arg(DIR_WINRAR)
                             .arg(code_packet)
                             .arg(dir.absolutePath());
             qDebug() <<extract_cmd;
             QProcess pro(0);
             pro.execute(extract_cmd);
-            m_loglist->addItem("3.解压完成.");
+            m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("解压完成."));
+            qApp->processEvents();
             dir.remove(code_packet);
-            m_loglist->addItem("4.删除原压缩包.");
+            m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("删除原压缩包."));
         }
 
     }
@@ -174,7 +197,18 @@ void MainWindow::on_m_start_clicked()
             modify_zhanhao(fileitem.fileName());
             //5.修改电压系数
             modify_vfactor(fileitem.fileName());
-            m_loglist->addItem("5.程序修改完成，请重新编译.");
+            //6.修改加解密
+            if (m_cb_jiemi->isChecked()){
+                modify_startup(true);
+            }else{
+                modify_startup(false);
+            }
+            m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("程序修改完成."));
+            qApp->processEvents();
+            //记录程序修改时间
+            m_hexFileTime = QDateTime::currentDateTime().toTime_t();
+
+            //on_m_build_clicked();
         }
 
     }
@@ -185,7 +219,7 @@ void MainWindow::on_m_start_clicked()
 
 void MainWindow::on_m_check_clicked()
 {
-    QFileInfo check_tool("C:\\Users\\Administrator\\Desktop\\ChangeName.exe");
+    QFileInfo check_tool(DIR_CHECK);
     qDebug()<<check_tool.absolutePath()<<m_code_path+"\\"+check_tool.fileName();
     QFile::copy(check_tool.filePath(), m_code_path+"\\"+check_tool.fileName());
 
@@ -198,13 +232,14 @@ void MainWindow::on_m_check_clicked()
 void MainWindow::on_m_compress_clicked()
 {
     QString code_dir = m_code_path+"\/"+m_dir_name;
-    QString packet_cmd = QString("WinRaR.exe a -ep1 -r -ibck -o+ %1 %2")
+    QString packet_cmd = QString("\"%1\" a -ep1 -r -ibck -o+ \"%2\" \"%3\"")
+                    .arg(DIR_WINRAR)
                     .arg(code_dir+".rar")
                     .arg(code_dir);
     qDebug() <<packet_cmd;
     QProcess pro(0);
     pro.execute(packet_cmd);
-    m_loglist->addItem("7.打包完成.");
+    m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("打包完成."));
 }
 
 void MainWindow::on_m_clear_clicked()
@@ -219,20 +254,11 @@ void MainWindow::on_m_clear_clicked()
 
 void MainWindow::on_m_copy_clicked()
 {
-    QDir dir(m_code_path + "\\" + m_dir_name + "\\Output\\LPC1788_TengHua_LPC1788_KEIL_CMSIS\\Release_FLASH\\Obj");
-    foreach(QFileInfo fileitem ,dir.entryInfoList())
-    {
+    //搜索编译日期之后的HEX文件，为编译完成的
+    if(build_check()){
 
-        if (fileitem.fileName().endsWith("hex") && fileitem.fileName().startsWith(m_hex_name))
-        {
-            if (QFile::copy(fileitem.filePath(), m_code_path+"\\"+fileitem.fileName()))
-            {
-                m_loglist->addItem("6.hex文件已拷贝到指定目录.");
-            }
-            qDebug()<<fileitem.filePath()<<m_code_path+"\\"+fileitem.fileName();
-            return;
-        }
-
+    }else{
+        QMessageBox::warning(this, "提示", "编译错误，请检查！");
     }
 }
 
@@ -274,7 +300,8 @@ void MainWindow::modify_global(QString name)
                 {
                     qDebug()<<strAll[i];
                     idx = strAll[i].indexOf("\"");
-                    strAll[i] = QString("%1%2\"").arg(strAll[i].left(idx+1)).arg(m_hex_name);
+                    if (!m_hex_name.isEmpty())
+                        strAll[i] = QString("%1%2\"").arg(strAll[i].left(idx+1)).arg(m_hex_name);
                     qDebug()<<strAll[i];
                 }
             }
@@ -292,8 +319,8 @@ void MainWindow::modify_global(QString name)
                     ck_code.replace("/","");
                     if (ck_code.count() == 10)
                         ck_code.insert(2,"0");
-
-                    strAll[i] = QString("%1%2\"").arg(strAll[i].left(idx-11)).arg(ck_code);
+                    if (!ck_code.isEmpty())
+                        strAll[i] = QString("%1%2\"").arg(strAll[i].left(idx-11)).arg(ck_code);
                     qDebug()<<strAll[i];
                 }
             }
@@ -315,7 +342,8 @@ void MainWindow::modify_global(QString name)
                             passwd = passwd.mid(2);
                         }
                     }
-                    strAll[i].replace(QRegExp("(\\d+)"),passwd);
+                    if (!passwd.isEmpty())
+                        strAll[i].replace(QRegExp("(\\d+)"),passwd);
                     qDebug()<<strAll[i];
                 }
             }
@@ -401,7 +429,7 @@ void MainWindow::modify_zhanhao(QString name)
                 qDebug()<<strAll[i];
                 strAll[i].replace(rx, ",1,");
                 qDebug()<<strAll[i];
-                m_loglist->addItem("站号修改完成.");
+                m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("站号修改完成."));
             }
 
             in << strAll[i]<<"\n";
@@ -441,7 +469,7 @@ void MainWindow::modify_vfactor(QString name)
                 qDebug()<<strAll[i];
                 strAll[i].replace("2000", "4000");
                 qDebug()<<strAll[i];
-                m_loglist->addItem("电压系数修改完成.");
+                m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("电压系数修改完成."));
             }
 
             in << strAll[i]<<"\n";
@@ -449,6 +477,162 @@ void MainWindow::modify_vfactor(QString name)
 
         file.close();
     }
+}
+
+void MainWindow::modify_startup(bool jiemi)
+{
+    QStringList strAll;
+    QString path = m_code_path + "/" + m_dir_name + "/System/HW/DeviceSupport/startup_LPC177x_8x_Keil.s";
+    if (!QFile::exists(path)){
+        return;
+    }
+    qDebug()<<path;
+    QFile file(path);
+    if(!file.open(QIODevice::ReadWrite))
+    {
+        qDebug()<<"Can't open the file!"<<endl;
+
+    }
+    QTextStream in(&file);
+    while(!in.atEnd())
+    {
+        QString line = in.readLine();
+        strAll.append(line);
+    }
+
+    file.resize(0);
+    in.reset();
+
+    for (int i=0;i<strAll.count();i++)
+    {
+        if (strAll[i].contains("CRP_Key         DCD    0xFFFFFFFF"))
+        {
+            qDebug()<<strAll[i];
+            if (jiemi){
+                strAll[i] = "CRP_Key         DCD    0xFFFFFFFF";
+            }else{
+                strAll[i] = ";CRP_Key         DCD    0xFFFFFFFF";
+            }
+            qDebug()<<strAll[i];
+        }
+        if (strAll[i].contains("CRP_Key         DCD    0x87654321"))
+        {
+            qDebug()<<strAll[i];
+            if (jiemi){
+                strAll[i] = ";CRP_Key         DCD    0x87654321";
+            }else{
+                strAll[i] = "CRP_Key         DCD    0x87654321";
+            }
+            qDebug()<<strAll[i];
+        }
+        in << strAll[i]<<"\n";
+    }
+
+    file.close();
+}
+
+void MainWindow::on_m_build_clicked()
+{
+    //http://www.keil.com/support/man/docs/uv4/uv4_commandline.htm
+
+    m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("开始编译..."));
+    qApp->processEvents();
+    //6.修改加解密
+    if (m_cb_jiemi->isChecked()){
+        modify_startup(true);
+    }else{
+        modify_startup(false);
+    }
+
+
+    QString code_path = m_code_path + "/" + m_dir_name;
+    //查找目录内工程文件后缀
+    //1.1判断路径是否存在
+    QDir dir(code_path);
+    if(!dir.exists())
+    {
+        return;
+    }
+    QFileInfoList InfoList = QDir(code_path).entryInfoList();//获取当前目录所有文件
+    QFileInfoList SuffixInfoList;//定义放提取文件的List
+    //遍历
+    foreach(QFileInfo fileInfo, InfoList )
+    {
+        if(!fileInfo.isFile()) continue;//不是文件继续，只用于加速，可不加
+        //后缀不区分大小写，需要区分直接用“==”
+        if(0==fileInfo.suffix().compare("uvproj", Qt::CaseInsensitive))
+        {
+            SuffixInfoList << fileInfo;//指定后缀，加入列表
+        }
+    }
+    if (InfoList.count()<0){
+        return;
+    }
+    m_hexFileTime = QDateTime::currentDateTime().toTime_t();
+    m_project_path = SuffixInfoList[0].absoluteFilePath();
+    //QString build_cmd = QString("%1 -j0 -r %2 -o build_log.txt").arg(DIR_KEIL).arg(project_path);
+    QString build_cmd = QString("\"%1\" -j0 -b \"%2\"").arg(DIR_KEIL).arg(m_project_path);
+    //这里的编译选项 -r:rebuild,-b:build
+    QProcess pro(0);
+    pro.execute(build_cmd);
+
+    m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("编译完成."));
+
+}
+
+void MainWindow::on_m_download_clicked()
+{
+    m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("开始下载..."));
+    qApp->processEvents();
+    QString down_cmd = QString("\"%1\" -j0 -f \"%2\"").arg(DIR_KEIL).arg(m_project_path);
+    QProcess pro(0);
+    pro.execute(down_cmd);
+    m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("下载完成."));
+}
+
+void MainWindow::on_build2_clicked()
+{
+    on_m_start_clicked();
+    on_m_build_clicked();
+    on_m_copy_clicked();
+}
+
+void MainWindow::on_clear2_clicked()
+{
+    on_m_compress_clicked();
+    on_m_clear_clicked();
+}
+
+
+
+int MainWindow::build_check()
+{
+    QString new_file;
+    QDir dir(m_code_path + "\\" + m_dir_name + "\\Output\\LPC1788_TengHua_LPC1788_KEIL_CMSIS\\Release_FLASH\\Obj");
+    foreach(QFileInfo fileitem ,dir.entryInfoList())
+    {
+        int hexFileTime = fileitem.lastModified().toTime_t();
+
+        if (fileitem.fileName().endsWith("hex")
+            && fileitem.fileName().startsWith(m_hex_name)
+            && (hexFileTime > m_hexFileTime))
+        {
+            //如果已经存在hex，则先删除
+            new_file = m_code_path+"\\"+fileitem.fileName();
+            if (QFileInfo::exists(new_file)){
+                QFile::remove(new_file);
+            }
+            if (QFile::copy(fileitem.filePath(), new_file))
+            {
+                m_loglist->addItem(QString("%1.%2").arg(m_loglist->count()).arg("已拷贝."));
+
+            }
+            qDebug()<<fileitem.filePath()<<m_code_path+"\\"+fileitem.fileName();
+            return 1;
+        }
+
+    }
+    return -1;
 }
 bool MainWindow::deleteDir(const QString &dirName)
  {
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,7 @@
 ﻿
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QCoreApplication>
 #include <QMainWindow>
 #include <QLabel>
 #include <QPushButton>
@@ -19,6 +19,13 @@
 #include <QProcess>
 #include <QFileDialog>
 #include <QListWidget>
+#include <QMessageBox>
+
+
+#define DIR_WINRAR      "C:\\Program Files\\WinRAR\\WinRAR.exe"
+#define DIR_CHECK       "C:\\Users\\Administrator\\Desktop\\ChangeName.exe"
+#define DIR_KEIL        "C:\\Keil_v5\\UV4\\UV4.exe"
+
 
 class MainWindow : public QMainWindow
 {
@@ -29,6 +36,8 @@ public:
     ~MainWindow();
 
     void setUi();
+    int build_check();
+
 private slots:
     void on_m_start_clicked();
     void on_m_check_clicked();
@@ -36,11 +45,18 @@ private slots:
     void on_m_compress_clicked();
     void on_m_clear_clicked();
     void on_btn_clicked();
+    void on_m_build_clicked();
+    void on_m_download_clicked();
+
+    void on_build2_clicked();
+    void on_clear2_clicked();
+
 
     void modify_global(QString name);
     void modify_hexfile(QString name);
     void modify_zhanhao(QString name);
     void modify_vfactor(QString name);
+    void modify_startup(bool jiemi);
 
     bool deleteDir(const QString &dirName);
 private:
@@ -58,18 +74,26 @@ private:
 
     QCheckBox *m_cb_vsd;
     QCheckBox *m_cb_vfactor;
+    QCheckBox *m_cb_jiemi;
 
     QPushButton *m_start;
+    QPushButton *m_build;
+    QPushButton *m_download;
     QPushButton *m_copy;
     QPushButton *m_check;
     QPushButton *m_compress;
     QPushButton *m_clear;
 
+
     QString m_code_path;
     QString m_hex_name;
     QString m_dir_name;
-
+    QString m_project_path;
+    int m_hexFileTime;
     QListWidget *m_loglist;
+
+    QPushButton *m_modfiy_build;
+    QPushButton *m_compress_clear;
 };
 
 #endif // MAINWINDOW_H
